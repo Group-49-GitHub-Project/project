9d1f683b13dcc7913d07baae933e221d877ff29f
haejeg <88898962+haejeg@users.noreply.github.com>
2023-11-19 19:14:06 -0700

    Update Dora.js

--- src/pages/Dora.js
+++ src/pages/Dora.js
@@ -6,23 +6,16 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Spider man. </h1>
+            <h1 className='title'>Hey, I'm Dora! </h1>
           </div>
           <img
             className='pic'
-            src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
-            alt='tom holland'
+            src='https://i.postimg.cc/GtMtKqJX/Dora-jumping-stock-art.webp'
+            alt='Dora'
           />
 
           <p className='description'>
-            Hey I am your friendly neighbourhood Spider Man. My real name is Tom
-            Holland. I am an actor but mostly I fight villains and juggle high school.
-            I am just your average student – well, maybe not so average. 
-            You see, I've got some incredible powers, like superhuman strength, agility, 
-            and the ability to shoot webs from my wrists. With great power comes great 
-            responsibility, and I'm here to protect New York City from all kinds of baddies,
-            from common crooks to supervillains. So, if you ever need a hand or a web-swinging
-            hero in your corner, just remember, I'm your friendly neighborhood Spider-Man!"
+          Dora the Explorer is a popular children’s television series that features Dora, a young Latina girl who loves to go on adventures with her monkey friend Boots and her talking backpack. The show teaches preschoolers basic skills such as counting, Spanish words, problem-solving, and teamwork. In each episode, Dora and Boots face challenges and obstacles along the way, and ask the viewers to help them by answering questions or repeating phrases.
           </p>
 
           <a className='button' href='/'>
